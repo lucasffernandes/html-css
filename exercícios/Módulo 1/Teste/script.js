bdcf233a26da43cb70dc4d59dfb36443253ba001
Lucas Fernandes <lucasfernandes1813@gmail.com>
2025-02-03 17:55:14 -0300

    Initial commit

---
+++ exercícios/Módulo 1/Teste/script.js
@@ -0,0 +1,14 @@
+const likeButtons = document.querySelectorAll('.like-btn');
+const chatButtons = document.querySelectorAll('.chat-btn');
+
+likeButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		// Add functionality to like button here
+	});
+});
+
+chatButtons.forEach((button) => {
+	button.addEventListener('click', () => {
+		// Add functionality to chat button here
+	});
+});
